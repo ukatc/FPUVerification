70e56dd8250d94be41f2699df6d8cb41b30ab7fc
Alan O'Brien <obrien.ad@googlemail.com>
2019-05-10 17:08:01 +0100

    Revert "Revert "Changing tests to use numpy testing for better error messages""
    This reverts commit ddd87c664e2b62c4ab0ad5de337d62eeadba4f88.

--- ImageAnalysisFuncs/Tests/test_PosRep.py
+++ ImageAnalysisFuncs/Tests/test_PosRep.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
 import unittest
+import numpy.testing as npt
 
 from ImageAnalysisFuncs.analyze_positional_repeatability import posrepCoordinates
 from vfr.conf import POS_REP_ANALYSIS_PARS
@@ -75,13 +76,13 @@ class TestPosRepImageAnalysis(unittest.TestCase):
             pos_limit = 0.01  # millimeter
             q_limit = 0.05  # dimensionless
 
-            self.assertTrue(abs(small_x - sx) < pos_limit)
-            self.assertTrue(abs(small_y - sy) < pos_limit)
-            self.assertTrue(abs(small_q - sq) < q_limit)
+            npt.assert_almost_equal(sx, small_x, pos_limit)
+            npt.assert_almost_equal(sy, small_y, pos_limit)
+            npt.assert_almost_equal(sq, small_q, q_limit)
 
-            self.assertTrue(abs(large_x - lx) < pos_limit)
-            self.assertTrue(abs(large_y - ly) < pos_limit)
-            self.assertTrue(abs(large_q - lq) < q_limit)
+            npt.assert_almost_equal(lx, large_x, pos_limit)
+            npt.assert_almost_equal(ly, large_y, pos_limit)
+            npt.assert_almost_equal(lq, large_q, q_limit)
 
 
 if __name__ == "__main__":
