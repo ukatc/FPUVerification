9f92501debe1f9bfcdcca4dbb9c158774cd8612c
Johannes Nix <johannes.nix@stfc.ac.uk>
2019-06-13 12:33:21 +0100

    factor out function to compute expected image coordinate

--- vfr/evaluation/eval_positional_verification.py
+++ vfr/evaluation/eval_positional_verification.py
@@ -11,6 +11,55 @@ from math import sin, cos
 import numpy as np
 
 
+def angle_to_point(
+        alpha_nom,
+        beta_nom,
+        coeffs=None,
+        x_center=None,
+        y_center=None,
+        R_alpha=None,
+        R_beta_midpoint=None,
+        alpha0=None,
+):
+
+    # these offsets make up for the rotation of
+    # the camera *and* for the +180 degree offset
+    # for beta in respect for the Cartesian system
+    a_alpha = coeffs['coeffs_alpha']['a']
+    a_beta = coeffs['coeffs_beta']['a']
+    print("offset alpha = ", np.rad2deg(a_alpha))
+    print("offset beta = ", np.rad2deg(a_beta))
+    print("R_alpha=", R_alpha)
+    print("R_beta_midpoint=", R_beta_midpoint)
+    if alpha0 is None:
+        # alpha reference point for deriving gamma
+        alpha0 = -180.3 + 5.0 # alpha_min + pos_rep_safety_margin
+
+    P0 = np.array([x_center, y_center])
+
+    # add offset from fitting
+    alpha = alpha_nom + np.rad2deg(a_alpha)
+    beta = beta_nom + np.rad2deg(a_beta)
+    # add difference to alpha when the beta
+    # correction was measured (these angles add up
+    # because when the alpha arm is turned (clockwise),
+    # this turns the beta arm (clockwise) as well).
+    gamma = beta + (alpha - alpha0)
+    # compute expected Cartesian coordinate of observation
+    pos_alpha = np.array(polar2cartesian(np.deg2rad(alpha), R_alpha))
+    pos_beta = np.array(polar2cartesian(np.deg2rad(gamma), R_beta_midpoint))
+
+    expected_point = P0 + pos_alpha + pos_beta
+    print("alpha_nom=%f, beta_nom=%f" % (alpha_nom, beta_nom))
+    print("alpha=%f, beta=%f, gamma=%f" % (alpha, beta, gamma))
+    print("p0=", P0)
+    print("p_expected=",expected_point)
+    print("p_a=", pos_alpha)
+    print("p_b=", pos_beta)
+
+    return expected_point
+
+
 def evaluate_positional_verification(
     dict_of_coords,
     pars=None,
@@ -57,46 +106,25 @@ def evaluate_positional_verification(
     # as the alpha arm is in exactly the same position relative to the
     # camera, so no changes to the camera or verification rig are
     # allowed!
-    P0 = np.array([x_center, y_center])
     expected_coords = []
     point_list = []
 
     print(">>>>>>>>>>>> computing point error values")
-    # these offsets make up for the rotation of
-    # the camera *and* for the +180 degree offset
-    # for beta in respect for the Cartesian system
-    a_alpha = coeffs['coeffs_alpha']['a']
-    a_beta = coeffs['coeffs_beta']['a']
-    print("offset alpha = ", np.rad2deg(a_alpha))
-    print("offset beta = ", np.rad2deg(a_beta))
-    print("R_alpha=", R_alpha)
-    print("R_beta_midpoint=", R_beta_midpoint)
-    if alpha0 is None:
-        # alpha reference point for deriving gamma
-        alpha0 = -180.3 + 5.0 # alpha_min + pos_rep_safety_margin
     for coords, point_pair in dict_of_coords.items():
         print("-------------")
         # get nominal coordinates
         (idx, alpha_nom, beta_nom) = coords
-        # add offset from fitting
-        alpha = alpha_nom + np.rad2deg(a_alpha)
-        beta = beta_nom + np.rad2deg(a_beta)
-        # add difference to alpha when the beta
-        # correction was measured (these angles add up
-        # because when the alpha arm is turned (clockwise),
-        # this turns the beta arm (clockwise) as well).
-        gamma = beta + (alpha - alpha0)
-        # compute expected Cartesian coordinate of observation
-        pos_alpha = np.array(polar2cartesian(np.deg2rad(alpha), R_alpha))
-        pos_beta = np.array(polar2cartesian(np.deg2rad(gamma), R_beta_midpoint))
+        expected_point = angle_to_point(
+            alpha_nom,
+            beta_nom,
+            coeffs=coeffs,
+            x_center=x_center,
+            y_center=y_center,
+            R_alpha=R_alpha,
+            R_beta_midpoint=R_beta_midpoint,
+            alpha0=alpha0,
+            )
 
-        expected_point = P0 + pos_alpha + pos_beta
-        print("alpha_nom=%f, beta_nom=%f" % (alpha_nom, beta_nom))
-        print("alpha=%f, beta=%f, gamma=%f" % (alpha, beta, gamma))
-        print("p0=", P0)
-        print("p_expected=",expected_point)
-        print("p_a=", pos_alpha)
-        print("p_b=", pos_beta)
         expected_coords.append(expected_point)
         point_list.append([point_pair])
         error_by_angle[coords] = get_errors(
