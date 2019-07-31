104d1e090b6c2a35a7f73b316f7aaf3c82855e96
Johannes Nix <johannes.nix@stfc.ac.uk>
2019-07-31 14:43:09 +0100

    change version numbers to 1.0.0 (now using semantic versioning)

--- vfr/db/datum_repeatability.py
+++ vfr/db/datum_repeatability.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function
 
 from collections import namedtuple
 from functools import partial
-from vfr.db.base import TestResult, save_named_record, get_named_record
+from vfr.db.base import TestResult, save_named_record, get_named_record, upgrade_version
 
 RECORD_TYPE = "datum-repeatability"
 
@@ -35,7 +35,9 @@ get_datum_repeatability_images = partial(get_named_record, (RECORD_TYPE, "images
 
 save_datum_repeatability_result = partial(save_named_record, (RECORD_TYPE, "result"))
 
-get_datum_repeatability_result = partial(get_named_record, (RECORD_TYPE, "result"))
+upgrade_func = partial(upgrade_version, fieldname="algorithm_version")
+
+get_datum_repeatability_result = partial(get_named_record, (RECORD_TYPE, "result"), upgrade_func=upgrade_func)
 
 
 def get_datum_repeatability_passed_p(dbe, fpu_id, count=None):
